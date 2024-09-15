078b45779d2264959c1f68307cced2b70292c6a6
mlaskowski7 <mtlaskowski7@gmail.com>
2024-09-15 23:19:31 +0200

    set up logging and main.r

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use log::info;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // init logger
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    info!("Starting the HTTP server on localhost:8080");
+
+    // run http server
+    HttpServer::new(|| {
+        App::new()
+    })
+    .bind("127.0.0.1:8080")?
+    .run().await
+
+
 }
