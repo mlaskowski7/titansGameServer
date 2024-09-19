6da8711978fd58a73755ea3f88ff18bb75b3e397
mlaskowski7 <mtlaskowski7@gmail.com>
2024-09-19 21:09:17 +0200

    update source sql query to create characters table

--- db_setup/create_tables.sql
+++ db_setup/create_tables.sql
@@ -1,10 +1,29 @@
 # drop tables if they exist
 DROP TABLE users;
+DROP TABLE characters;
 
 # create tables
+CREATE TABLE characters (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    health INT,
+    strength INT
+);
+
+INSERT INTO characters (name, health, strength)
+VALUES
+    ('Warrior', 100, 80),
+    ('Mage', 70, 90),
+    ('Rogue', 85, 75),
+    ('Paladin', 120, 60);
+
 CREATE TABLE users (
    id INT AUTO_INCREMENT PRIMARY KEY,
    username VARCHAR(255) NOT NULL,
    password VARCHAR(255) NOT NULL,
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   times_logged_in INT,
+   character_id INT,
+   FOREIGN KEY (character_id) REFERENCES characters(id)
 );
+
